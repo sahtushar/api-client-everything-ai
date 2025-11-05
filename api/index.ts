71c7a21a89ba7d8e5c8ad6931326ba5deb3259d5
Tushar | तुषार <tushar.sah@zeni.ai>
2025-11-06 00:06:26 +0530

    Update TypeScript configuration to include API directory and remove Vercel serverless function setup

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,10 @@
 /**
  * Vercel Serverless Function Entry Point
  * This file is used by Vercel to deploy the Express app as serverless functions
+ * Vercel will automatically compile this TypeScript file
  */
 
-import app from "../dist/app.js";
+import app from "../src/app.js";
 
 // Export the Express app as the default export for Vercel
 export default app;
